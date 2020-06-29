ef99bd2f8ba143dda125f3eb95dcb315a70163df
Andres Rojas <afro17@gmail.com>
2020-06-29 12:33:32 -0500

    Initial project

---
+++ UIsplasher/UIsplasherApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIsplasherApp.swift
+//  UIsplasher
+//
+//  Created by Andres Rojas on 29/06/20.
+//
+
+import SwiftUI
+
+@main
+struct UIsplasherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
